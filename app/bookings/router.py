d646d054a617d04c846d25c9498cd2eaaf2c29c0
IoannD <IoannD>
2023-07-20 19:16:39 +0300

    Revert "added endpoint: 'add_booking'"
    This reverts commit 44a7dd16e9184f0117072a7feb8b1174a32a2abf.

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,24 +1,12 @@
-from fastapi import APIRouter, Depends
-from datetime import date
+from fastapi import APIRouter
 
 from app.bookings.dao import BookingDAO
 from app.bookings.shemas import SBooking
-from app.users.models import Users
-from app.users.dependencies import get_current_user
-from app.exception import RoomCannotBeBookedException
+
 
 router = APIRouter(prefix='/bookings', tags=['Bookings'])
 
 
 @router.get('')
-async def get_bookings(user: Users =
-                       Depends(get_current_user)) -> list[dict[str, SBooking]]:
-    return await BookingDAO.find_all(user_id=user.id)
-
-
-@router.post('')
-async def add_booking(room_id: int, date_from: date, date_to: date,
-                      user: Users = Depends(get_current_user)):
-    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
-    if not booking:
-        raise RoomCannotBeBookedException
+async def get_bookings() -> list[dict[str, SBooking]]:
+    return await BookingDAO.find_all()
